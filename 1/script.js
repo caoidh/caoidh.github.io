be4d9f1193f0f5a2473d51c5251a2e91b2d3992d
caoidh <107569233+caoidh@users.noreply.github.com>
2023-07-22 18:09:09 +0300

    Create script.js

---
+++ 1/script.js
@@ -0,0 +1,19 @@
+const items = ["карандаш", "ручка", "клавиатура", "телефон", "книга", "ноутбук"];
+let secretItem = items[Math.floor(Math.random() * items.length)];
+let attempts = 3;
+
+function checkGuess() {
+  const guess = document.getElementById("guess").value.toLowerCase();
+  const result = document.getElementById("result");
+
+  if (guess === secretItem) {
+    result.textContent = "Поздравляем! Вы угадали!";
+  } else {
+    attempts--;
+    if (attempts === 0) {
+      result.textContent = `Увы, вы проиграли. Загаданный предмет был: ${secretItem}.`;
+    } else {
+      result.textContent = `Неверно. У вас осталось ${attempts} попытки.`;
+    }
+  }
+}
